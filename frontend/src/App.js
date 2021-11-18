304cbc4b448fccd2aaa59eab0c5207cd35ec9b74
kseniya <kseniyamudrakovadev@gmail.com>
2021-11-18 17:48:22 +0100

    install Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <></>;
+  return (
+    <div className="myApp">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h1>Welcome to Groupommania</h1>
+        <div className="container"></div>
+      </main>
+    </>
+  );
+}
+
+export default Home;
