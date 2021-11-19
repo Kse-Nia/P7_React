7327628d00739992944abcc319123a5d520e6e25
kseniya <kseniyamudrakovadev@gmail.com>
2021-11-19 21:48:27 +0100

    change front UI + add routes backend

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./Components.css";
-import Logo from "../pages/Medias/icon-left-font.svg";
+import Logo from "../pages/Medias/icon-left-font.png";
 
 function Navbar() {
   return (
     <div className="Navbar">
       <a href="/" className="Link">
         Accueil
-      </a>{" "}
+      </a>
       <a href="/register" className="Link">
         S 'inscrire
       </a>
       <a href="/login" className="Link">
         Se connecter
       </a>
-      <div className="logo">
-        <img src={Logo} alt="Groupomania logo" />
+      <div className="Logo">
+        <img src={Logo} className="Navbarlogo" alt="Groupomania logo" />
       </div>
     </div>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,28 +1,63 @@
 import * as React from "react";
 import "./Home.css";
 import Social from "./Medias/socialpicture.jpg";
+import Sharing from "./Medias/sharing.jpg";
 
 function Home() {
   return (
     <>
       <main>
-        <h1> Bienvenue sur Groupomania </h1>
-        <div className="container">
-          <div className="presentation">
-            <p>Lorem ipsum</p>
-            <p>Lorem ipsum</p>
-            <p>Lorem ipsum</p>
+        <div className="Container">
+          <div className="Container1">
+            <div className="presentation">
+              <h1> Bienvenue sur Groupomania </h1>
+              <p>
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+                consectetur, adipisci velit..."
+              </p>
+              <br />
+              <p>
+                "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+                consectetur, adipisci velit..."
+              </p>
+            </div>
+            <div className="presentation">
+              <img
+                className="HomePicture"
+                src={Social}
+                alt="social network picture"
+              />
+            </div>
+            <div className="container-socialpicture"></div>
           </div>
-          <div className="presentation">
-            <img
-              className="HomePicture"
-              src={Social}
-              alt="social network picture"
-            />
+          <div className="Container2">
+            <div className="Information">
+              <h3>Lorem Ipsum</h3>
+              <img src={Sharing} alt="" className="InformationPicture" />
+              <div className="BlocText">
+                <p>Lorem Ipsum</p>
+                <p>Lorem Ipsum lorem Ipsum lorem</p>
+              </div>
+            </div>
+            <div className="Information">
+              <h3>Lorem Ipsum</h3>
+              <img src={Sharing} alt="" className="InformationPicture" />
+              <div className="BlocText">
+                <p>Lorem Ipsum</p>
+                <p>Lorem Ipsum lorem Ipsum lorem</p>
+              </div>
+            </div>
+            <div className="Information">
+              <h3>Lorem Ipsum</h3>
+              <img src={Sharing} alt="" className="InformationPicture" />
+              <div className="BlocText">
+                <p>Lorem Ipsum</p>
+                <p>Lorem Ipsum lorem Ipsum lorem</p>
+              </div>
+            </div>
           </div>
-          <div className="container-socialpicture"></div>
         </div>
-      </main>{" "}
+      </main>
     </>
   );
 }
