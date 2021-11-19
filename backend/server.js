5972b0502e15ed89b745b4dc5392dfcb03093cdf
kseniya <kseniyamudrakovadev@gmail.com>
2021-11-19 17:05:43 +0100

    modif font UI

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'secret',
+    database: 'my_db'
+});
+
 
 app.listen(3001, (req, res) => {
     console.log("The server is running..");
